ca3abdde86e67fd0854e020fc096a163e1232ed7
Ruslan <113594889+Leon13Mozart@users.noreply.github.com>
2022-09-17 20:13:36 +0300

    Add files via upload

---
+++ lab3(Zad 1).py
@@ -0,0 +1,4 @@
+a = float(input("Введите a: "))
+b = float(input("Введите b: "))
+S=1/2*a*b
+print("S=",S)
---
+++ lab3(zad 2).py
@@ -0,0 +1,6 @@
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+n = a+b+c/3
+print("g=",n)
